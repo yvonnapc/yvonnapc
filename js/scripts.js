5c4979ce1f221a7f3c7e7d8c45d70692274d811e
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-10-24 15:59:52 -0700

    Begin individual project pages

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $("#read-blog1").click(function(){
     $("#show-test").toggle();
   });
+
   $("#mobile-menu").click(function(){
     $(".mobile-nav").toggle();
   });
