4aac4ffa7e63992aaf1146d8c62ceb9acc441970
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-10-31 13:33:05 -0700

    mobile blog

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-  $("#read-blog1").click(function(){
-    $("#show-test").toggle();
-  });
-
   $("#mobile-menu").click(function(){
     $(".mobile-nav").toggle();
   });
